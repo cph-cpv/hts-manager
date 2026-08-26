2731a5d148fee93069fd65c12783689dba504bbb
ReeceHoffmann <reecehoffmann@gmail.com>
2026-08-26 09:01:33 -0700

    refactor(config): validate transfers in zod schema
    - Validate transfer dependencies and filesystem paths during environment parsing
    - Transform the parsed environment into the typed application config

--- src/server/database-plugin.ts
+++ src/server/database-plugin.ts
@@ -1,11 +1,11 @@
 import { definePlugin } from 'nitro'
 import { migrateDatabase } from '../db/db'
 import { ensureWorkersStarted } from './bootstrap'
-import { readTransferConfig } from './config'
+import { getConfig } from './config'
 
 /** Prepare storage and start background workers before Nitro accepts requests. */
 export default definePlugin(() => {
-  const transferConfig = readTransferConfig()
+  const { transfer } = getConfig()
   migrateDatabase()
-  ensureWorkersStarted(transferConfig)
+  ensureWorkersStarted(transfer)
 })
